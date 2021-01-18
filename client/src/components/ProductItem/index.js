3dbe75b18c4e477ffdb3cc8961de310202ba9fbd
galacticnative <68270151+galacticnative@users.noreply.github.com>
2021-01-17 20:12:14 -0700

    card height

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -46,7 +46,7 @@ function ProductItem(item) {
   return (
     <div className="card px-1 py-1">
       <Link to={`/products/${_id}`}>
-        <img
+        <img className="imgCard"
           alt={name}
           src={`/images/${image}`}
         />
